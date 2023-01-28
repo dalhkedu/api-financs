6a35e226f983eb169b0f499ef8f7ce7613d86ccc
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2023-01-27 21:11:46 -0300

    removendo arquivos nao usados e add requirements

--- app/calculo_preco_produto.py
+++
@@ -1,8 +0,0 @@
-# https://www.sebrae-sc.com.br/blog/como-calcular-o-preco-de-um-produto
-# CD (Custos Diretos) + CI (Custos Indiretos) + CF (Custos Fixos) + CV (Custos Variáveis) = CT (Custo Total).
-class CalcularPrecoProduto:
-    def __init__(self, custo_direto, custo_indireto, custo_fixo, custo_variavel):
-        self.custo_direto = custo_direto
-        self.custo_indireto = custo_indireto
-        self.custo_fixo = custo_fixo
-        self.custo_variavel = custo_variavel
